ffa09fb334851812c5bfac9352e09a0cf0d935eb
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-03-16 16:54:34 +0330

    add classname "selected" for Paragraphs

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -23,17 +23,17 @@ function Pagination({ page, setPage }) {
       >
         previous
       </button>
-      <p style={{ color: page === 1 ? "red" : "inherit" }}>1</p>
-      <p style={{ color: page === 2 ? "red" : "inherit" }}>2</p>
+      <p className={page === 1 ? styles.selected : null}>1</p>
+      <p className={page === 2 ? styles.selected : null}>2</p>
       {page > 2 && page < 9 && (
         <>
           <span>...</span>
-          <p>{page}</p>
+          <p className={styles.selected}>{page}</p>
         </>
       )}
       <p>...</p>
-      <p>9</p>
-      <p>10</p>
+      <p className={page === 9 ? styles.selected : null}>9</p>
+      <p className={page === 10 ? styles.selected : null}>10</p>
       <button
         onClick={nextHandler}
         className={page === 10 ? styles.disabled : null}
