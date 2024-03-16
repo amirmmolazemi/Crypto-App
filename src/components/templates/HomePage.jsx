296c629f3d10c13c8ce9ca1a5c92ff5b2848f7fd
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-03-16 17:39:01 +0330

    fix: add search component and pass some props and fix fetch

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -2,27 +2,30 @@ import { useState, useEffect } from "react";
 import { getCoinList } from "../../services/cryptoApi.js";
 import TableCoin from "../modules/TableCoin.jsx";
 import Pagination from "../modules/Pagination.jsx";
+import Search from "../modules/Search.jsx";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState();
   const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("usd");
 
   useEffect(() => {
     const getData = async () => {
       setIsLoading(true);
-      const res = await fetch(getCoinList(page));
+      const res = await fetch(getCoinList(page, currency));
       const json = await res.json();
       setCoins(json);
       setIsLoading(false);
     };
 
     getData();
-  }, [page]);
+  }, [page, currency]);
 
   return (
     <>
-      <TableCoin coins={coins} isLoading={isLoading} />
+      <Search setCurrency={setCurrency} currency={currency} />
+      <TableCoin coins={coins} isLoading={isLoading} currency={currency} />
       <Pagination page={page} setPage={setPage} />
     </>
   );
