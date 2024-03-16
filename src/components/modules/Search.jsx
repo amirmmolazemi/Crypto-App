3be15f5981cd621b8df8e93075ddb8eaadc9b213
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-03-17 00:43:30 +0330

    feat: add search feature into project

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,12 +1,77 @@
+import { useEffect, useState } from "react";
+import { searchCoin } from "../../services/cryptoApi";
+import { RotatingLines } from "react-loader-spinner";
+import styles from "./Search.module.css";
+
 function Search({ currency, setCurrency }) {
+  const [text, setText] = useState("");
+  const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setCoins([]);
+    if (!text) {
+      setIsLoading(false);
+      return;
+    }
+
+    const search = async () => {
+      try {
+        const res = await fetch(searchCoin(text), {
+          signal: controller.signal,
+        });
+        const json = await res.json();
+        if (json.coins) {
+          setCoins(json.coins);
+          setIsLoading(false);
+        } else {
+          alert(json.status.error_message);
+        }
+      } catch (error) {
+        if (error.message !== "AbortError") {
+          console.log(error.message);
+        }
+      }
+    };
+    setIsLoading(true);
+    search();
+
+    return () => controller.abort();
+  }, [text]);
   return (
-    <div>
-      <input type="text" />
+    <div className={styles.searchBox}>
+      <input
+        type="text"
+        placeholder="Search"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
         <option value="usd">USD</option>
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
+      {(!!coins.length || isLoading) && (
+        <div className={styles.searchResults}>
+          {isLoading && (
+            <RotatingLines
+              width="50px"
+              height="50px"
+              strokeWidth="2"
+              strokeColor="#3874ff"
+            />
+          )}
+          <ul>
+            {coins.map((coin) => (
+              <li key={coin.id}>
+                <img src={coin.thumb} alt={coin.name} />
+                <p>{coin.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
