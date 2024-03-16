3a7bb393ae20a2609bf1ca8edbe8fd10c5052701
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-03-17 00:42:58 +0330

    handling use effect errors

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -11,12 +11,16 @@ function HomePage() {
   const [currency, setCurrency] = useState("usd");
 
   useEffect(() => {
+    setIsLoading(true);
     const getData = async () => {
-      setIsLoading(true);
-      const res = await fetch(getCoinList(page, currency));
-      const json = await res.json();
-      setCoins(json);
-      setIsLoading(false);
+      try {
+        const res = await fetch(getCoinList(page, currency));
+        const json = await res.json();
+        setCoins(json);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
     };
 
     getData();
