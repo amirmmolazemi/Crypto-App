ffed008380e83011f145ffbf398821e1bf7bf803
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-03-17 20:27:53 +0330

    replace some tags

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,17 +1,19 @@
+import styles from "./Layout.module.css";
+
 function Layout({ children }) {
   return (
     <>
-      <div>
+      <header className={styles.header}>
         <h1>Crypto App</h1>
         <p>
           <a href="https://github.com/amirmmolazemi">Amirmmolazemi</a>| react
           course practice
         </p>
-      </div>
-      <div>{children}</div>
-      <div>
+      </header>
+      <main>{children}</main>
+      <footer className={styles.footer}>
         <p>Developed by Amirmmolazemi with ❤️</p>
-      </div>
+      </footer>
     </>
   );
 }
