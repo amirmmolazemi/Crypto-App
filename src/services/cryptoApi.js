5f49b18062fd965daaf711087eb9353a72435e83
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-03-17 09:50:53 +0330

    add a function for chart api

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -5,7 +5,12 @@ const getCoinList = (page, currency) => {
   return `${BASE_URL}coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`;
 };
 
-const searchCoin = (query) =>
-  `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
+const searchCoin = (query) => {
+  return `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
+};
 
-export { getCoinList, searchCoin };
+const marketChart = (id) => {
+  return `${BASE_URL}/coins/${id}/market_chart?vs_currency=usd&days=7&x_cg_demo_api_key=${API_KEY}`;
+};
+
+export { getCoinList, searchCoin, marketChart };
