ab2efacc247a011731e5beb9c833b41f852e5c64
amirmmolazemi <amirmohamad.molazemi@gmail.com>
2024-03-17 09:50:23 +0330

    pass the props from parent component to child component

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -3,12 +3,14 @@ import { getCoinList } from "../../services/cryptoApi.js";
 import TableCoin from "../modules/TableCoin.jsx";
 import Pagination from "../modules/Pagination.jsx";
 import Search from "../modules/Search.jsx";
+import Chart from "../modules/Chart.jsx";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState();
   const [page, setPage] = useState(1);
   const [currency, setCurrency] = useState("usd");
+  const [chart, setChart] = useState(null);
 
   useEffect(() => {
     setIsLoading(true);
@@ -29,8 +31,14 @@ function HomePage() {
   return (
     <>
       <Search setCurrency={setCurrency} currency={currency} />
-      <TableCoin coins={coins} isLoading={isLoading} currency={currency} />
+      <TableCoin
+        coins={coins}
+        isLoading={isLoading}
+        currency={currency}
+        setChart={setChart}
+      />
       <Pagination page={page} setPage={setPage} />
+      {!!chart && <Chart chart={chart} setChart={setChart}/>}
     </>
   );
 }
